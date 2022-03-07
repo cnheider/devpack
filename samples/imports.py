d406a361e8b497d10ddba4098083567a5d40e19c
Christian Heider Nielsen <christian.heider@alexandra.dk>
2022-03-07 14:41:42 +0100

    ok

--- samples/imports.py
+++ samples/imports.py
@@ -5,3 +5,8 @@ import warg
 print(warg.__version__)
 print(draugr.__version__)
 print(apppath.__version__)
+
+
+# TOOD: https://www.python.org/dev/peps/pep-0440/#pre-releases
+# pip install --pre xxx
+# Include pre-release and development versions. By default, pip only finds stable versions.
